fcd80d4e694adef6248680a094228ba505e77240
Jon Penton <hey@jondpenton.com>
2023-05-17 20:35:46 -0400

    feat: :arrow_up: upgraded GraphQL-related packages

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import 'reflect-metadata'
-import { ApolloServer } from 'apollo-server'
-import { ApolloServerPlugin } from 'apollo-server-plugin-base'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
 import { buildSchema, ResolverData } from 'type-graphql'
 import { Awilix } from './lib/awilix'
 import { Logger } from './logger'
 import { RecipeResolver } from './recipe/recipe.resolver'
 import { sampleRecipes } from './recipe/recipe.samples'
 import { RecipeService } from './recipe/recipe.service'
-import { Context } from './types'
+import { Context, Cradle } from './types'
 
-const container = Awilix.createContainer().register({
+const container = Awilix.createContainer<
+  Pick<Cradle, 'logger' | 'recipeService' | 'sampleRecipes'>
+>().register({
   logger: Awilix.asClass(Logger),
   recipeService: Awilix.asClass(RecipeService).classic(),
   sampleRecipes: Awilix.asValue(sampleRecipes),
@@ -31,34 +33,36 @@ async function main() {
   })
 
   // create GraphQL server
-  const server = new ApolloServer({
-    schema,
-    // we need to provide unique container with `requestId` for each request
-    context: (): Context => {
-      const requestId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) // uuid-like
-      const context = {
-        container: container.createScope().register({
-          requestId: Awilix.asValue(requestId),
-        }),
-      }
-
-      return context
-    },
+  const server = new ApolloServer<Context>({
     // create a plugin that will allow for disposing the scoped container created for every request
     plugins: [
       {
         requestDidStart: async () => ({
           async willSendResponse(requestContext) {
             // remember to dispose the scoped container to prevent memory leaks
-            await requestContext.context.container.dispose()
+            await requestContext.contextValue.container.dispose()
           },
         }),
       },
-    ] as ApolloServerPlugin<Context>[],
+    ],
+    schema,
   })
 
   // start the server
-  const { url } = await server.listen(4000)
+  const { url } = await startStandaloneServer(server, {
+    // we need to provide unique container with `requestId` for each request
+    context: async () => {
+      const requestId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) // uuid-like
+      const context = {
+        container: container.createScope<Pick<Cradle, 'requestId'>>().register({
+          requestId: Awilix.asValue(requestId),
+        }),
+      }
+
+      return context
+    },
+    listen: { port: 4000 },
+  })
   console.log(`Server is running, GraphQL Playground available at ${url}`)
 }
 
